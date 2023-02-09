dd2bfed654f70148db96870cd5877ba5bf969527
thegreatest1ever <mugzdzed@gmail.com>
2023-02-09 19:16:32 +0300

    Demo Commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+export const Footer = (props) => {
+  
+  return (
+    <div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2023 Pelah's Glam. Design by{' '}
+            <a href='http://www.github.com/thegreatest1ever' rel='nofollow'>
+              Github
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
